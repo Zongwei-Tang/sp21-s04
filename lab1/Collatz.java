cb446174d3550f7c4582d0d767c8942bef4ccffe
Zongwei Tang <tangzongwei070509@gmail.com>
2025-05-01 05:17:48 +0800

    Reset Collatz.java to skeleton version

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,14 @@
  */
 public class Collatz {
 
-    public static Integer nextNumber(int n) {
-        if (n == 1){
-            return null;
-        }
-        else if (n % 2 == 0){
-            return n / 2;
-        }
-        else {
+    /** Buggy implementation of nextNumber! */
+    public static int nextNumber(int n) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
 
